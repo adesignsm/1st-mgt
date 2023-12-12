5d621de1cbab17c28de7b730b432ef89d2c03b51
Akash Mulye <akashmulye97@gmail.com>
2023-12-12 13:32:12 -0500

    added girlsclub model schema, and integrated into component

--- 1st-mgt-cms/schemas/index.js
+++ 1st-mgt-cms/schemas/index.js
@@ -1,7 +1,9 @@
 import popUp from "./popUp"
 import firstMgt from "./firstMgt"
+import girlsClubModels from "./girlsClubModels"
 
 export const schemaTypes = [
     popUp,
     firstMgt,
+    girlsClubModels,
 ]
--- src/Routes/GirlsClubInTown/index.jsx
+++ src/Routes/GirlsClubInTown/index.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react';
+import sanityClient from '../../client';
+import ImageUrlBuilder from '@sanity/image-url';
+
+import './index.css';
+
 const GirlsClubInTown = () => {
+    const [models, setModels] = useState([]);
+
+    const builder = ImageUrlBuilder(sanityClient);
+
+    const urlFor = (source) => {
+      return builder.image(source);
+    }
+  
+    const fetchData = async () => {
+      try {
+        const query = `*[_type == 'girlsClubModels' && defined(modelStats) && defined(modelPictures)] {
+            modelStats,
+            modelPictures
+        }`;
+        const result = await sanityClient.fetch(query);
+        setModels(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    console.log(models);
+
     return (
         <>
-            <h1> test </h1>
+            
         </>
     )
 }
