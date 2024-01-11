e96c30379e5370b4d75600fe1a2b3c947d094e94
Akash Mulye <akashmulye97@gmail.com>
2024-01-11 14:28:59 -0500

    added news cms

---
+++ 1st-mgt-cms/schemas/newsPosts.js
@@ -0,0 +1,50 @@
+export default {
+    name: 'newsPosts',
+    title: 'News Posts',
+    type: 'document',
+    description: 'Data Bank for all Newws Posts',
+    fields: [
+        {
+            name: 'postTitle',
+            type: 'string',
+            title: 'Post & Slice Title',
+            description: 'This is the title for this News Post. *Note: This will be displayed in the "View All" list as well as act as the title for the post itself.".'
+        },
+        {
+            name: 'postDetails',
+            title: 'Post Details (models, brands, etc)',
+            type: 'object',
+            description: 'This is where you can add the names of the models in this post, and the brands that were involved.',
+            fields: [
+                {
+                    name: 'modelNames',
+                    title: 'Model Names',
+                    type: 'array',
+                    description: 'Add names of the models invovled in this post',
+                    of: [{ type: 'string' }],
+                    options: {
+                        layout: 'default'
+                    }
+                },
+            ]
+        },
+        {
+            name: 'postMedia',
+            title: 'Post Media',
+            type: 'array',
+            description: 'Add any media content related to this post.',
+            of: [
+                {
+                    type: 'image',
+                    options: {
+                        hotspot: true
+                    }
+                },
+                {
+                    type: 'file',
+                    title: 'Video'
+                }
+            ]
+        }
+    ]
+}
